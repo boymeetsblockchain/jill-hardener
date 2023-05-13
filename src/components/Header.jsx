aa8258ba42bc1ebf617fca3b30de76c188f950c5
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-05-13 16:32:17 +0100

    added the navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import {BsFillBellFill} from 'react-icons/bs'
+import profile from '../assets/profile.jpg'
+import React, { useState, useEffect } from 'react';
+import toast from 'react-hot-toast'
+export default function Header() {
+    const [timeOfDay, setTimeOfDay] = useState('');
+
+    useEffect(() => {
+      const getCurrentTimeOfDay = () => {
+        const currentHour = new Date().getHours();
+  
+        if (currentHour >= 5 && currentHour < 12) {
+          setTimeOfDay('Morning');
+        } else if (currentHour >= 12 && currentHour < 18) {
+          setTimeOfDay('Afternoon');
+        } else {
+          setTimeOfDay('Evening');
+        }
+      };
+  
+      getCurrentTimeOfDay();
+    }, []);
+    const onClick = ()=>{
+        toast.success("No new notification")
+    }
+  return (
+    <div className='text-white px-2 py-10'>
+     <div className="flex flex-col">
+      <div className="flex justify-between items-center">
+       <div className="profile flex  gap-1">
+         <div className="img">
+            <img src={profile} alt="" className='h-12 w-12 rounded-lg' />
+         </div>
+         <div className="name-good">
+            <h1 className='text-sm mb-1'>Jill Hardener</h1>
+            <h1 className='text-sm text-gray-400'>Good {timeOfDay}</h1>
+         </div>
+        </div>
+      <div className="bell">
+         <BsFillBellFill size={24} className='text-white hover:text-lime-300' onClick={onClick}/>
+       </div>
+       </div>
+       <div className="balance flex flex-col my-6 gap-3">
+         <p className='text-sm text-gray-400'>Total Balance:</p>
+          <h1 className='text-lime-300 text-2xl'>$42,295.00 USD</h1>
+          <p className='text-red-400 text-[8px]'>Account restricted, please contact your management</p>
+       </div>
+       <hr  className='text-gray-400'/>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import  {FaCreditCard } from 'react-icons/fa'
+import {BiTransferAlt} from 'react-icons/bi'
+import {TbSmartHome} from 'react-icons/tb'
+import {BsPersonFill} from 'react-icons/bs'
+import toast from 'react-hot-toast'
+
+export default function Navbar() {
+    const onClick = ()=>{
+        toast.error("Not authorized")
+    }
+  return (
+  <nav className='fixed bottom-0 left-0 right-0 p-4  text-white  bg-transparent rounded-md'>
+      <ul className="flex flex-row justify-between">
+         <div className="home flex flex-col items-center hover:border-t-2 border-lime-300">
+            <TbSmartHome size={24}  className='hover:text-lime-300 mt-1' />
+         <h2>Home</h2>
+         </div>
+         <div className="home flex flex-col items-center hover:border-t-2 border-lime-300"  onClick={onClick}>
+            <FaCreditCard size={24}  className='hover:text-lime-300 mt-1'/>
+         <h2>Card</h2>
+         </div>
+         <div className="home flex flex-col items-center hover:border-t-2 border-lime-300"  onClick={onClick}>
+            <BiTransferAlt size={24}  className='hover:text-lime-300 mt-1'/>
+         <h2>Transaction</h2>
+         </div>
+         <div className="home flex flex-col items-center hover:border-t-2 border-lime-300 "  onClick={onClick}>
+         <BsPersonFill size={24}  className='hover:text-lime-300 mt-1'/>
+         <h2>Profile</h2>
+         </div>
+      </ul>
+  </nav>
+  )
+}
