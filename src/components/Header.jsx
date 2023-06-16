52f255a9e939bb8c67b5c599d5e688de182598af
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-06-16 17:21:45 +0100

    default

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { BsFillBellFill } from 'react-icons/bs';
 import profile from '../assets/profile.jpg';
-import profile2 from '../assets/profile2.jpg'
 import React, { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
 
@@ -33,10 +32,10 @@ export default function Header() {
         <div className="flex justify-between items-center">
           <div className="profile flex gap-1">
             <div className="img">
-              <img src={profile2} alt="" className='h-12 w-12 rounded-lg' />
+              <img src={profile} alt="" className='h-12 w-12 rounded-lg' />
             </div>
             <div className="name-good">
-              <h1 className='text-sm mb-1'>Kayla Moody</h1>
+              <h1 className='text-sm mb-1'>Jill Hardener</h1>
               <h1 className='text-sm text-gray-400'>Guten {timeOfDay}</h1>
             </div>
           </div>
@@ -46,7 +45,7 @@ export default function Header() {
         </div>
         <div className="balance flex flex-col my-6 gap-3">
           <p className='text-sm text-gray-400'>Gesamtsaldo:</p>
-          <h1 className='text-lime-300 text-2xl'> €90,000 EUR</h1>
+          <h1 className='text-lime-300 text-2xl'> €700,000 EUR</h1>
           <p className='text-red-400 text-[8px]'>Konto eingeschränkt, bitte kontaktieren Sie das Management</p>
         </div>
         <hr className='text-gray-400' />
